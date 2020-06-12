4a446c0d77afbf86b5016e06bc74c00b167b7192
Alexander Courtis <alex@courtis.org>
2020-06-12 10:05:23 +1000

    allow indexed colours but do not specify them

--- x.c
+++ x.c
@@ -800,7 +800,7 @@ xloadcols(void)
 		for (cp = dc.col; cp < &dc.col[dc.collen]; ++cp)
 			XftColorFree(xw.dpy, xw.vis, xw.cmap, cp);
 	} else {
-		dc.collen = 16;
+		dc.collen = MAX(LEN(colour), 256);
 		dc.col = xmalloc(dc.collen * sizeof(Color));
 	}
 
